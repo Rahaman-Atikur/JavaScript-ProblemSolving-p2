00bffcdedac137a1709c7444e4e99710f1f09857
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-18 08:24:10 +0100

    Solved the problem, issues was to update the object. Any one could update the object after comparing the particular paramenter or a number , So min was changed and it shouled returned.

--- objectMax.js
+++ objectMax.js
@@ -1,22 +1,29 @@
-let mobile = [
+/**
+ * find the minimum price from a mobile phone list
+ * -------------------------------------------------------------------------
+ * 
+ */
+
+
+let mobiles = [
     { name: 'Samsung', price: 18000, color: 'black' },
     { name: 'Iphone', price: 20000, color: 'white' },
     { name: 'Google', price: 25000, color: 'purple' },
     { name: 'Vivo', price: 28000, color: 'pink' },
     { name: 'Xiuami', price: 29000, color: 'blue' },
     { name: 'Oppo', price: 31000, color: 'Navyblue' },
-    { name: 'Nokia', price: 35000, color: 'red' }
+    { name: 'Nokia', price: 35000, color: 'red' },
 ]
-function cheapFinder(mobile) {
-    let min = mobile[0];
-    let cheap;
-    for (const myShopping of mobile) {
+function cheapFinder(mobiles) {
+    let min = mobiles[0];
+    for (const mobile of mobiles) {
         if (mobile.price < min.price) {
-             min = mobile;
+            min = mobile;
         }
     }
-    return cheap;
+    return min;
 }
 
-console.log(cheapFinder(mobile));
+let result = cheapFinder(mobiles);
+console.log(result.name);
 
