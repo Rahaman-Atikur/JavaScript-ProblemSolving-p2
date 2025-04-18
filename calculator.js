3b86ce111e30c461c16e6dc2d4505785cc297857
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-04-18 10:40:04 +0100

    calculator done

---
+++ calculator.js
@@ -0,0 +1,38 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subs(num1, num2) {
+    return num1 - num2;
+}
+function mul(num1, num2) {
+    return num1 * num2;
+}
+function div(num1, num2) {
+    return num1 / num2;
+}
+let operation;
+function calculator(num1, num2,operation) {
+    if (operation === 'add') {
+        console.log(add(num1, num2));
+
+    }
+    else if (operation === 'subs') {
+        console.log(subs(num1, num2));
+
+    }
+    else if (operation === 'mul') {
+        console.log(mul(num1, num2));
+
+    }
+    else if (operation === 'div') {
+        console.log(div(num1, num2));
+
+    }
+    
+
+}
+calculator(2,3,'subs');
+
+
+
+
